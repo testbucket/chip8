2d990b0410aab40383cbb4aee8d2de47d93030d1
Diarmuid <diarmuid.moloney@gmail.com>
2021-01-30 14:42:50 +0000

    sdfgh

---
+++ src/chip8.rs
@@ -0,0 +1,28 @@
+use crate::ram::Ram;
+use crate::cpu::Cpu;
+
+pub(crate) struct Chip8 {
+    ram: Ram,
+    cpu: Cpu
+}
+
+impl Chip8 {
+    pub fn new() -> Chip8 {
+        Chip8 {
+            ram: Ram::new(),
+            cpu: Cpu::new()
+        }
+    }
+    pub fn load_rom(&mut self, data: &Vec<u8>) {
+        print!("loading rom");
+        for i in 0..data.len() {
+
+
+            self.ram.write_byte((0x200 + i) as u16, data[i])
+        }
+    }
+
+    pub fn run_instruction(& mut self){
+        self.cpu.run_instruction (&mut self.ram)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod ram;
+mod chip8;
+mod cpu;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+use crate::ram::Ram;
+use crate::chip8::Chip8;
+use crate::cpu::Cpu;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut file = File::open("INVADERS").unwrap();
+    let mut data = Vec::<u8>::new();
+    file.read_to_end(&mut data);
+
+    let mut chip8 = Chip8::new();
+    chip8.load_rom (&data);
+
+    loop {
+        chip8.run_instruction();
+    }
+    print!("hello")
 }
